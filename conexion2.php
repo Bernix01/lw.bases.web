f5a952e54bf4c90a6d303218d8caa85fe8d58c9c
soccerdroid <mabegc@gmail.com>
2016-12-15 10:32:02 -0500

    dfs

---
+++ conexion2.php
@@ -0,0 +1,26 @@
+<?php
+$conn = mysqli_connect("localhost","root","root");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+else 
+    { echo "Connection successful <br>";
+}
+$query="select count(*) from information_schema.SCHEMATA";
+$dbnum=mysqli_query($conn,$query);
+if($dbnum){
+    $data = mysqli_fetch_array($dbnum);
+    echo $data[0];
+}   
+else{
+    die('Invalid query:'.mysqli_error());
+}
+
+
+
+mysqli_close($conn);
+
+?>
