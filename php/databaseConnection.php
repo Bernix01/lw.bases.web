6999d6b14a8eb2bafe6b10f92d6be4186cd0bbdb
soccerdroid <mabegc@gmail.com>
2016-12-29 19:30:51 -0500

    conexion oo y colector general

--- php/databaseConnection.php
+++ php/databaseConnection.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect("localhost","root","root");
+
 	class DatabaseException extends Exception
 	{
 	}
@@ -10,8 +10,8 @@
 		private $host="localhost";
 		private $user="root";
 		private $password="root";
-		$port = NULL;
-	    $socket = NULL;
+		private $port = NULL;
+	  private $socket = NULL;
 			private $con = false; // Check to see if the connection is active
 
 
@@ -31,21 +31,7 @@
 
 	        throw new DatabaseException($message);
 	    }
-			public function disconnect(){
-    	// If there is a connection to the database
-    	if($this->con){
-    		// We have found a connection, try to close it
-    		if($this->myconn->close()){
-    			// We have successfully closed the connection, set the connection variable to false
-    			$this->con = false;
-				// Return true tjat we have closed the connection
-				return true;
-			}else{
-				// We could not close the connection, return false
-				return false;
-			}
-		}
-    }
+
 	}
 
 ?>
