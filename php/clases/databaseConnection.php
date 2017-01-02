3125557b829b76ff1ad3e44f05f22f454b3751e0
Bernix01 <gbernal096@gmail.com>
2017-01-02 11:10:59 -0500

    cambio de charset en base datos y reordenamiento del reordenamiento.

--- php/clases/databaseConnection.php
+++ php/clases/databaseConnection.php
@@ -1,46 +1,44 @@
 <?php
 
-	class DatabaseException extends Exception
-	{
-	}
-
-	class DatabaseConnection extends Mysqli
-	{
-		private $database="lw";
-		private $host="localhost";
-		private $user="root";
-		private $password="root";
-		private $port = NULL;
-	  private $socket = NULL;
-			private $con = false; // Check to see if the connection is active
-
-
-
-	    public function __construct() {
-
-	        parent::__construct($this->host, $this->user, $this->password, $this->database, $this->port, $this->socket);
-					$this->con=true;
-					if (!$mysqli->set_charset("utf8")) {
-    printf("Error loading character set utf8: %s\n", $mysqli->error);
-    exit();
-} else {
-    printf("Current character set: %s\n", $mysqli->character_set_name());
+class DatabaseException extends Exception
+{
 }
-	        $this->throwConnectionExceptionOnConnectionError();
-	    }
 
-	    private function throwConnectionExceptionOnConnectionError() {
+class DatabaseConnection extends Mysqli
+{
+    private $database = "lw";
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "root";
+    private $port = NULL;
+    private $socket = NULL;
+    private $con = false; // Check to see if the connection is active
 
-	        if (!$this->connect_error) return;
 
-	        $message = sprintf('(%s) %s', $this->connect_errno, $this->connect_error);
+    public function __construct()
+    {
 
-	        throw new DatabaseException($message);
-	    }
-		public function getDBname(){
-			return $this->database;
-		}
+        parent::__construct($this->host, $this->user, $this->password, $this->database, $this->port, $this->socket);
+        $this->con = true;
+        $this->set_charset("utf8");
+        $this->throwConnectionExceptionOnConnectionError();
+    }
 
-	}
+    private function throwConnectionExceptionOnConnectionError()
+    {
+
+        if (!$this->connect_error) return;
+
+        $message = sprintf('(%s) %s', $this->connect_errno, $this->connect_error);
+
+        throw new DatabaseException($message);
+    }
+
+    public function getDBname()
+    {
+        return $this->database;
+    }
+
+}
 
 ?>
