87e883d1e18be82e92ff74d2ad69bccd82bb7c6d
NovaStriker <johnaldoc@hotmail.com>
2017-01-05 13:58:25 -0500

    bjkbk
    ???

--- php/clases/infocursoColector.php
+++ php/clases/infocursoColector.php
@@ -18,11 +18,36 @@ class InfocursoColector{
         return NULL;
     }
 
+<<<<<<< HEAD:php/clases/infocursoColector.php
     public function addInfocurso($id_curso,$infocurso){
         $query= "INSERT INTO info_curso VALUE ($id_curso,$infocurso->get_descripcion(),$infocurso->get_cupo_min(),$infocurso->get_cupo_max,$infocurso->get_cupos_disponibles(),$infocurso->get_fecha_inicio(),$infocurso->get_fecha_fin())";
+=======
+    public function addInfoCurso($id_curso,$nombre,$descripcion,$cupo_min,$cupo_max,$cupos_disponibles,$fecha_inicio,$fecha_fin){
+        $query= "INSERT INTO info_curso(cupo_min,cupo_max,cupos_disponibles,$echa_inicio,fecha_fin) VALUE ($id_curso,$infocurso->getDescripcion(),$infocurso->get_cupo_min(),$infocurso->get_cupo_max,$infocurso->get_cupos_disponibles(),$infocurso->get_fecha_inicio(),$infocurso->get_fecha_fin())";
+>>>>>>> origin/master:php/clases/infoCursoColector.php
         $result=$this->worker->query($query);
-        $data=mysqli_fetch_object($result,'Info_curso'); //count(array)(?)
-        return $data;
+        if($result!==null){
+          $data=mysqli_fetch_object($result,'Info_curso'); //count(array)(?)
+          return $data;
+        }
+
+        return null;
+    }
+    public public function updateInfoCurso($id_curso,$nombre,$descripcion,$cupo_min,$cupo_max,$cupos_disponibles,$fecha_inicio,$fecha_fin)
+    {
+      $query="UPDATE info_curso SET descripcion=$descripcion, cupo_min=$cupo_min, cupo_max=$cupo_max, cupos_disponibles=$cupos_disponibles, fecha_inicio=$fecha_inicio, fecha_fin=$fecha_fin WHERE id_curso=$id_curso";
+      $result=$this->worker->query($query);
+      return $result!==null;
+    }
+    public function deleteInfoCurso($id){
+      $query="DELETE FROM info_curso WHERE id_curso=$id";
+      $result=$this->worker->query($query);
+      if($result!==null){
+        return true;
+      }
+      else{
+        return false;
+      }
     }
 }
 ?>
