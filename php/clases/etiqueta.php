a055dd40dee430d4f001fc799afecf9739e97c5d
soccerdroid <mabegc@gmail.com>
2017-01-05 14:27:06 -0500

    fsdfa

---
+++ php/clases/etiqueta.php
@@ -0,0 +1,35 @@
+<?php
+  /**
+   *
+   */
+  class Etiqueta
+  {
+    private $id_etiqueta;
+    private $nombre;
+
+    function __construct($id_etiqueta=null,$nombre=null)
+    {
+      if($id_etiqueta!==null){
+          $this->id_etiqueta=$id_etiqueta;
+      }
+      if($nombre!==null){
+        $this->nombre=$nombre;
+      }
+
+    }
+
+    public function get_id_etiqueta(){
+      return $this->id_etiqueta;
+    }
+    public function set_id_etiqueta($id){
+      $this->id_etiqueta=$id;
+    }
+    public function get_nombre(){
+      return $this->nombre;
+    }
+    public function set_nombre($nombre){
+      $this->nombre=$nombre;
+    }
+  }
+
+ ?>
