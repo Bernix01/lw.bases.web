0e111b9b55b85d0afc3924c95b4b24c56df4c6c4
soccerdroid <mabegc@gmail.com>
2017-01-07 22:54:51 -0500

    arreglando conflictos de colectores

--- php/clases/emprendimientoColector.php
+++ php/clases/emprendimientoColector.php
@@ -1,34 +1,5 @@
 <?php
-<<<<<<< HEAD
-	include_once('colector.php');
-	include_once ('emprendimiento.php');
-	class EmprendimientoColector{
-		private $worker=NULL;
 
-		public function __construct(){
-			$this->worker= new Colector();
-		}
-
-		public function getEmprendimientoById($id){
-			$query= "SELECT * FROM emprendimiento WHERE emprendimiento.id_emprendimiento=".$id." limit 1";
-			$result=$this->worker->query($query);
-			if($result!==NULL){
-				$data=mysqli_fetch_object($result,'Emprendimiento');//count(array(?)
-				return $data;
-			}
-			return NULL;
-		}
-
-		public function addEmprendimiento($emprendimiento){
-			$query= "INSERT INTO emprendimiento VALUE (DEFAULT(),$emprendimiento->get_nombre_emp(),$emprendimiento->get_descripcion_emp(); SELECT LAST_INSERT_ID();";
-			$result=$this->worker->query($query);
-			$nuevo_id = (mysqli_fetch_assoc($result));
-			$curso->set_id_emprendimiento($nuevo_id["id_emprendimiento"]);
-			return $emprendimiento;
-		}
-	}
-?>
-=======
 include_once('colector.php');
 include_once ('Emprendimiento.php');
   class EmprendimientoColector{
@@ -98,4 +69,3 @@ include_once ('Emprendimiento.php');
     }
   }
 ?>
->>>>>>> origin/master
