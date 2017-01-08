c2525a73a8d0b4330f9ca906b4fd3f5d418c74be
soccerdroid <mabegc@gmail.com>
2017-01-08 00:39:26 -0500

    editado colector de pago

--- php/clases/pagoColector.php
+++ php/clases/pagoColector.php
@@ -19,32 +19,45 @@ class PagoColector{
       return NULL;
     }
 
-	public function addPagoTarjeta($id_pago,$forma_pago,$n_tarjeta)
-	{
-		$info = new Pago($id_pago,$forma_pago,null,$n_tarjeta,null);
-		$query= "INSERT INTO pago(id_pago,forma_pago,n_tarjeta) VALUES ($id_pago,$forma_pago,$n_tarjeta)";
-		$result=$this->worker->query($query);
-		if($result!==null){
-      $id_pago=$this->worker->query("SELECT LAST_INSERT_ID()");
-			return $this->getPagoById($id_pago);
-		}
-		return null;
-	}
+  	public function addPagoTarjeta($id_pago,$forma_pago,$n_tarjeta)
+  	{
+  		$info = new Pago($id_pago,$forma_pago,null,$n_tarjeta,null);
+  		$query= "INSERT INTO pago(id_pago,forma_pago,n_tarjeta) VALUES ($id_pago,$forma_pago,$n_tarjeta)";
+  		$result=$this->worker->query($query);
+  		if($result!==null){
+        $id_pago=$this->worker->query("SELECT LAST_INSERT_ID()");
+  			return $this->getPagoById($id_pago);
+  		}
+  		return null;
+  	}
 
-	public function addPagoDeposito($id_pago,$forma_pago,$id_factura,$n_deposito)
-	{
-		$info = new Pago($id_pago,$forma_pago,$id_factura,null,$n_deposito);
-		$query= "INSERT INTO Pago(id_pago,forma_pago,id_factura,n_deposito) VALUES ($id_pago,$forma_pago,$id_factura,$n_deposito)";
-		$result=$this->worker->query($query);
-		if($result!==null){
-			return $this->getPagoById($id_pago);
-		}
-		return null;
-	}
-  public function deletePagoTarjeta($value='')
-  {
-    # code...
-  }
+  	public function addPagoDeposito($id_pago,$forma_pago,$id_factura,$n_deposito)
+  	{
+  		$info = new Pago($id_pago,$forma_pago,$id_factura,null,$n_deposito);
+  		$query= "INSERT INTO Pago(id_pago,forma_pago,id_factura,n_deposito) VALUES ($id_pago,$forma_pago,$id_factura,$n_deposito)";
+  		$result=$this->worker->query($query);
+  		if($result!==null){
+  			return $this->getPagoById($id_pago);
+  		}
+  		return null;
+  	}
+    public function deletePago($id)
+    {
+      $query="DELETE FROM pago WHERE id_pago=$id";
+      $result=$this->worker->query($query);
+      if($result!==null){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    public public function updatePago($id,$id_factura,$forma_pago,$n_tarjeta,$n_deposito)
+    {
+      $query="UPDATE pago SET id_factura=$id_factura, forma_pago=$forma_pago, n_tarjeta=$n_tarjeta n_deposito=$n_deposito WHERE id_pago=$id";
+      $result=$this->worker->query($query);
+      return $result!==null;
+    }
 
   }
 ?>
